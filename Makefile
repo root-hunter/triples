1690eb8683964edc80f14ee66f3fe9af3d38a44b
roothunter <antonio.ricciardi99@outlook.it>
2026-01-05 19:53:55 +0100

    optimize gcd from euclide to binary version

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+# Makefile for building and running examples
+
+# Benchmark Example
 benchmark-clean:
 	cargo clean --package benchmark
 
@@ -6,3 +9,19 @@ benchmark-build:
 
 benchmark-run:
 	cargo run --release --package benchmark
+
+benchmark: benchmark-build
+	cd target/release && ./benchmark
+
+# Big Limits Example
+big-limits-clean:
+	cargo clean --package big-limits
+
+big-limits-build:
+	cargo build --release --package big-limits
+
+big-limits-run:
+	cargo run --release --package big-limits
+
+big-limits: big-limits-build
+	cd target/release && ./big-limits
--- triples/src/euclide.rs
+++ triples/src/euclide.rs
@@ -1,22 +1,51 @@
 use std::io::Write;
 
+// Binary GCD algorithm
+// Get it from https://github.com/frewsxcv/rust-gcd/blob/master/src/lib.rs
 #[inline(always)]
-fn gcd(mut a: usize, mut b: usize) -> usize {
-    while b != 0 {
-        let tmp = b;
-        b = a % b;
-        a = tmp;
+fn gcd(mut u: usize, mut v: usize) -> usize {
+    if u == 0 {
+        return v;
     }
-    a
+    if v == 0 {
+        return u;
+    }
+
+    let shift = (u | v).trailing_zeros();
+    u >>= shift;
+    v >>= shift;
+    u >>= u.trailing_zeros();
+
+    loop {
+        v >>= v.trailing_zeros();
+
+        #[allow(clippy::manual_swap)]
+        if u > v {
+            // mem::swap(&mut u, &mut v);
+            let temp = u;
+            u = v;
+            v = temp;
+        }
+
+        v -= u; // here v >= u
+
+        if v == 0 {
+            break;
+        }
+    }
+
+    u << shift
 }
 
 pub fn stream_triples(limit: usize, count: &mut usize, mut buf: Option<&mut impl Write>) {
     *count = 0;
-    let m_max = ((limit - 1) as f64).sqrt() as usize;
+    let m_max = ((limit as f64).sqrt() as usize) + 1;
 
     for m in 2..=m_max {
         let m2 = m * m;
-        if m2 + 1 >= limit {
+
+        // Early exit se anche la tripla più piccola con questo m supera il limite
+        if m2 + 1 > limit {
             break;
         }
 
@@ -24,37 +53,20 @@ pub fn stream_triples(limit: usize, count: &mut usize, mut buf: Option<&mut impl
         let start_n = if m & 1 == 0 { 1 } else { 2 };
 
         for n in (start_n..m).step_by(2) {
-            // Ensure m and n have opposite parity
-            if (m & 1) == (n & 1) {
-                continue;
-            }
+            if gcd(m, n) == 1 {
+                let n2 = n * n;
+                let a = m2 - n2;
+                let b = 2 * m * n;
+                let c = m2 + n2;
 
-            if gcd(m, n) != 1 {
-                continue;
-            }
+                let k_max = limit / c;
+                *count += k_max;
 
-            // Generate primitive triple
-            let n2 = n * n;
-            let a = m2 - n2;
-            let b = 2 * m * n;
-            let c = m2 + n2;
-
-            // Generate multiples of the primitive triple
-            // and store them if c <= N
-
-            let mut ka = a;
-            let mut kb = b;
-            let mut kc = c;
-
-            while kc <= limit {
                 if let Some(out) = buf.as_deref_mut() {
-                    writeln!(out, "{}^2 + {}^2 = {}^2", ka, kb, kc).unwrap();
+                    for k in 1..=k_max {
+                        writeln!(out, "{}^2 + {}^2 = {}^2", a * k, b * k, c * k).unwrap();
+                    }
                 }
-                *count += 1;
-
-                ka += a;
-                kb += b;
-                kc += c;
             }
         }
     }
