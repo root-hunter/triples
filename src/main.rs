a2b2fd359594498dd97e10fbeaab7b0816a9676e
roothunter <antonio.ricciardi99@outlook.it>
2026-01-05 13:59:11 +0100

    stable

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,44 @@
-#![feature(portable_simd)]
-use std::simd::{Mask, Simd, StdFloat, cmp::{SimdPartialEq, SimdPartialOrd}};
+const N: usize = 10000;
 
-// const N: usize = 8192;
-const N: usize = 8192 * 2;
-const LANE: usize = 8;
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
 
 fn main() {
     let mut mat = Vec::with_capacity(N * 2);
     let start_time = std::time::Instant::now();
 
-    let n_simd = Simd::splat(N as f64);
+    for m in 2.. {
+        let m2 = m * m;
+        for n in 1..m {
+            if (m - n) % 2 == 1 && gcd(m, n) == 1 {
+                let n2 = n * n;
+                let a = m2 - n2;
+                let b = 2 * m * n;
+                let c = m2 + n2;
 
-    for a in 2..N {
-        let x = (a * a) as f64;
-        let x_simd = Simd::splat(x);
-
-        let mut b = a;
-        while b <= N {
-            // batch di 8 b
-            let mut b_values = [0f64; LANE];
-            let mut max_b = LANE;
-
-            for i in 0..LANE {
-                if b + i < N {
-                    b_values[i] = (b + i) as f64;
-                } else {
-                    b_values[i] = 0.0;
-                    max_b = i;
+                if c >= N {
                     break;
                 }
-            }
 
-            let b_simd = Simd::<f64, 8>::from_array(b_values);
-            let c2_simd = b_simd * b_simd + x_simd;
-            let c_simd = c2_simd.sqrt();
-
-            // Maschera per verificare quali c sono interi e <= n
-            let mask = (c_simd.fract().simd_eq(Simd::splat(0.0))) 
-                & (c_simd.simd_lt(n_simd)) & b_simd.simd_lt(c_simd);
-
-            let c_array = c_simd.to_array();
-            let b_array = b_simd.to_array();
-
-            for i in 0..LANE {
-                if mask.test(i) && i < max_b {
-                    mat.push([a, b_array[i] as usize, c_array[i] as usize]);
+                let mut k = 1;
+                while k * c <= N {
+                    mat.push([k * a, k * b, k * c]);
+                    k += 1;
                 }
             }
-
-            b += LANE;
+        }
+        if m2 + 1 >= N {
+            break;
         }
     }
 
+
     for t in &mat {
-        println!("{}^2+{}^2={}^2", t[0], t[1], t[2]);
     }
 
     let duration = start_time.elapsed();
